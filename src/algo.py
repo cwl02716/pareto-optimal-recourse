ddb1aa98a0249690625e8996d17c57c89cc4a2ef
cwl02716 <winnie2002824@gmail.com>
2024-03-14 18:20:49 +0800

    cost function

--- src/algo.py
+++ src/algo.py
@@ -17,7 +17,27 @@ def adj_to_graph(A: csr_matrix) -> pd.DataFrame:
     return graph
 
 
-def cost(df: pd.DataFrame, i: int, j: int) -> tuple[float, float]: ...
+def cost(df: pd.DataFrame, i: int, j: int) -> tuple[float, float]:
+    time = 0
+    payment = 0
+    # for age
+    if (df.loc[j, "age"] - df.loc[i, "age"]>= 0):
+        time.max(time, df.loc[j, "age"] - df.loc[i, "age"]) 
+    else:
+        time += 0
+    # education
+    if (df.loc[j, "education-num"] - df.loc[i, "education-num"]>= 0):
+        time.max(time, df.loc[j, "education-num"] - df.loc[i, "education-num"])
+    else:
+        time += 0
+    # hours-per-week and workclass
+    t = df.loc[j, "workclass"] - df.loc[i, "workclass"]
+    time += t if t > 0 else 0
+    p = df.loc[j, "hours-per-week"] - df.loc[i, "hours-per-week"]/t
+    # do sigmoid to p
+    payment += 1/(1+np.exp(-p))
+    # gain and loss
+    payment += (df.loc[j, "capital-gain"] - df.loc[i, "capital-gain"]) + (df.loc[i, "capital-loss"] - df.loc[j, "capital-loss"])
 
 
 def merge(i: int, j: int): ...
