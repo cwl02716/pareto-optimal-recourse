e6150eaac010c3797039cddd401087cd9bf402af
Stanley5249 <seer852741@gmail.com>
2024-04-08 20:41:51 +0800

    refactor: recourse-adult

--- src/helper.py
+++ src/helper.py
@@ -1,118 +1,29 @@
 from pathlib import Path
 from typing import Any
 
-from matplotlib import pyplot as plt
 import pandas as pd
-import sklearn
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import MinMaxScaler
 
-__all__ = ["load_dataframe", "transfrom_dataframe", "YCOL"]
-
-YCOL = "50K"
-
-
-def _select_same(
-    df: pd.DataFrame,
-    index: int,
-    immutables: list[Any] = [
-        "race",
-        "sex",
-        "native-country",
-    ],
-) -> pd.DataFrame:
-    X = df[immutables]
-    i = X.eq(X.iloc[index]).all(1)
-    return df[i].drop(columns=immutables)
-
-
-def _fit_minmax(X: pd.DataFrame) -> MinMaxScaler:
-    scaler = MinMaxScaler()
-    scaler.fit(X)
-    return scaler
-
-
-def _fit_kmeans(
-    X: pd.DataFrame,
-    k: int,
-    seed: Any = None,
-) -> KMeans:
-    kmeans = KMeans(k, random_state=seed)
-    kmeans.fit(X)
-    return kmeans
-
-
-def _fit_knn(X: pd.DataFrame, y: pd.Series, k: int) -> KNeighborsClassifier:
-    knn = KNeighborsClassifier(k)
-    knn.fit(X, y)
-    return knn
-
-
-def _fit_pca(X: pd.DataFrame, n_components: int = 2) -> PCA:
-    pca = PCA(n_components)
-    pca.fit(X)
-    return pca
-
-
-def show_path(df: pd.DataFrame, path: list[int], pca: PCA):
-    X = df.drop(columns=YCOL)
-    X_path = X.iloc[path]
-    X_pca = pca.transform(X_path)
-    plt.plot(X_pca[:, 0], X_pca[:, 1], "o-")
-    plt.show()
+__all__ = ["load_dataframe", "select_rows_by_immutables"]
 
 
 def load_dataframe(
-    path: str = "dataset/50Ktrain.csv",
-    drops: list[Any] = [
-        "fnlwgt",
-        "education",
-        "marital-status",
-        "relationship",
-        "occupation",
-    ],
+    path: str,
+    drops: list[Any] = [],
 ) -> pd.DataFrame:
     df = pd.read_csv(Path(path))
-    df.drop(columns=drops, inplace=True)
+    if drops:
+        df = df.drop(columns=drops)
     return df
 
 
-def transfrom_dataframe(
+def select_rows_by_immutables(
     df: pd.DataFrame,
     index: int,
-    size: int,
-    *,
-    seed: Any = None,
-) -> tuple[MinMaxScaler, pd.DataFrame, int]:
-    df = _select_same(df, index)
-    X = df.drop(columns=YCOL)
-    y = df[YCOL]
-
-    scaler = _fit_minmax(X)
-    X_scaled: pd.DataFrame = scaler.transform(X)  # type: ignore
-
-    kmeans = _fit_kmeans(X_scaled, size, seed)
-    X_small = pd.DataFrame(
-        kmeans.cluster_centers_,
-        columns=X_scaled.columns,
-    )
-
-    knn = _fit_knn(X_scaled, y, 5)
-
-    pred = knn.predict(X_small)
-    y_small = pd.DataFrame(pred, columns=[YCOL])
-
-    df_small = pd.concat((X_small, y_small), axis=1)
-    index_small = kmeans.predict(X_scaled.iloc[[index]]).item()
-
-    return scaler, df_small, index_small
-
-
-sklearn.set_config(transform_output="pandas")
-
-if __name__ == "__main__":
-    df = load_dataframe()
-    scaler, df_new, index_new = transfrom_dataframe(df, 0, 100, seed=42)
-    print(index_new)
+    immutables: list[Any] = [],
+) -> pd.DataFrame:
+    X = df[immutables]
+    i = X.eq(X.iloc[index]).all(1)
+    df = df[i]
+    if immutables:
+        df = df.drop(columns=immutables)
+    return df
---
+++ src/recourse-adult.py
@@ -0,0 +1,87 @@
+from typing import Any
+
+import pandas as pd
+import sklearn
+from algo import backtracking, recourse
+from helper import load_dataframe, select_rows_by_immutables
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
+
+PATH = "dataset/50Ktrain.csv"
+
+DROPS = ["fnlwgt", "education", "marital-status", "relationship", "occupation"]
+
+IMMUTABLES = ["race", "sex", "native-country"]
+
+YCOL = "50K"
+
+
+def transform(
+    df: pd.DataFrame,
+    index: int,
+    size: int,
+    k: int,
+    *,
+    seed: Any = None,
+) -> tuple[MinMaxScaler, pd.DataFrame, int]:
+    X = df.drop(columns=YCOL)
+    y = df[YCOL]
+
+    scaler = MinMaxScaler()
+    scaler.fit(X)
+    X_scaled: pd.DataFrame = scaler.transform(X)  # type: ignore
+
+    kmeans = KMeans(size, random_state=seed)
+    kmeans.fit(X_scaled)
+    X_small = pd.DataFrame(kmeans.cluster_centers_, columns=X.columns)
+
+    knn = KNeighborsClassifier(k)
+    knn.fit(X_scaled, y)
+
+    y_small = pd.DataFrame(knn.predict(X_small), columns=[YCOL])
+    df_small = pd.concat((X_small, y_small), axis=1)
+    index_small = kmeans.predict(X_scaled.iloc[[index]]).item()
+
+    return scaler, df_small, index_small
+
+
+def show_path(
+    df: pd.DataFrame,
+    path: list[int],
+    pca: PCA,
+) -> None:
+    ax: plt.Axes  # type: ignore
+    fig, ax = plt.subplots()
+    X = df.drop(columns=YCOL)
+    y = df[YCOL]
+    X_pca: pd.DataFrame = pca.transform(X)  # type: ignore
+    X_path = X_pca.iloc[path]
+    ax.plot("pca0", "pca1", "k:", data=X_path)
+    ax.scatter("pca0", "pca1", c=y, data=X_pca)
+    plt.show()
+
+
+def main(index: int, size: int, k: int, limit: int, *, seed: int) -> None:
+    df = load_dataframe(PATH, DROPS)
+
+    df = select_rows_by_immutables(df, index, IMMUTABLES)
+
+    scalar, df_small, s = transform(df, index, size, k, seed=seed)
+
+    graph, ts, dists = recourse(df_small, k, s, limit=limit, verbose=False)
+
+    paths = backtracking(graph, dists, s, size)
+
+    pca = PCA(2)
+    pca.fit(df_small.drop(columns=YCOL))
+    if paths:
+        show_path(df_small, paths[0], pca)
+
+
+sklearn.set_config(transform_output="pandas")
+
+if __name__ == "__main__":
+    main(0, 100, 3, 100, seed=42)
