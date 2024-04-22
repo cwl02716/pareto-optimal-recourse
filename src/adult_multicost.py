658d5d9f36ff589b9d090eef8e9dcfd6c6bbf564
Stanley5249 <seer852741@gmail.com>
2024-04-22 13:24:49 +0800

    refactor: move functinos from example to helper

--- src/adult_multicost.py
+++ src/adult_multicost.py
@@ -1,17 +1,17 @@
 import math
 from functools import partial
-from typing import Protocol
 from warnings import warn
 
 import fire
-import numpy as np
 import pandas as pd
 import seaborn as sns
 import sklearn
-from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
 from helper.adult import (
+    get_targets,
     load_dataframe,
+    non_outliers_mask,
+    plot_images,
+    proba_argsort,
     select_actionable,
 )
 from helper.algorithm import (
@@ -23,33 +23,16 @@ from helper.algorithm import (
 )
 from helper.cmd import fire_cmd
 from helper.common import select_indices, select_mask, select_samples
-from matplotlib import patheffects
 from matplotlib import pyplot as plt
-from numpy.typing import NDArray
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 sklearn.set_config(transform_output="pandas")
 sns.set_style("white")
 sns.set_context("paper")
 sns.set_palette("bright")
-
-
-class SupportsProba(Protocol):
-    def predict_proba(self, X) -> NDArray: ...
-
-
-def non_outliers_mask(X: pd.DataFrame, threshold: float) -> NDArray:
-    return (X.abs() <= threshold).all(1).to_numpy()
-
-
-def proba_argsort(model: SupportsProba, X: pd.DataFrame) -> NDArray:
-    return model.predict_proba(X)[:, 1].argsort()
-
-
-def get_targets(y: pd.Series, label: int) -> list[int]:
-    return (y == label).to_numpy().nonzero()[0].tolist()
 
 
 def multi_costs_fn(X: pd.DataFrame, i: int, j: int) -> MultiCost:
@@ -83,84 +66,6 @@ def multi_costs_fn(X: pd.DataFrame, i: int, j: int) -> MultiCost:
     payment += 1.0 / (1.0 + 1.44 * math.exp(m))  # add bias
 
     return MultiCost((AdditionCost(time), AdditionCost(payment)))
-
-
-def plot_images(
-    X: pd.DataFrame,
-    y: pd.Series,
-    paths: list[list[int]],
-    component: PCA,
-    model: SupportsProba,
-) -> None:
-    ax: plt.Axes  # type: ignore
-    fig, ax = plt.subplots()
-    X_2d: pd.DataFrame = component.transform(X)  # type: ignore
-    ft1, ft2 = component.get_feature_names_out()
-    sns.scatterplot(
-        X_2d,
-        x=ft1,
-        y=ft2,
-        hue=y,
-        hue_norm=(0, 1),
-        size=y,
-        sizes=(6, 24),
-        size_order=(1, 0),
-        style=y,
-        ax=ax,
-        palette="coolwarm",
-        alpha=0.9,
-        ec="w",
-        lw=0.2,
-        zorder=len(paths) + 2,
-    )
-
-    bound = ax.axis()
-    step = 256
-    j = complex(0, step)
-    grid = np.mgrid[bound[0] : bound[1] : j, bound[2] : bound[3] : j]
-    X_grid = pd.DataFrame(
-        component.inverse_transform(grid.reshape(2, step * step).T),
-        columns=component.feature_names_in_,
-    )
-    y_grid = model.predict_proba(X_grid)[:, 1].reshape(step, step)
-    ct = ax.contourf(
-        grid[0],
-        grid[1],
-        y_grid,
-        levels=10,
-        vmin=0,
-        vmax=1,
-        alpha=1 / 3,
-        zorder=0,
-        cmap="coolwarm",
-    )
-    cb = fig.colorbar(
-        ct,
-        ax=ax,
-        label="Probability",
-    )
-    cb.solids.set_alpha(1)  # type: ignore
-
-    for i, path in enumerate(paths):
-        X_path = X_2d.iloc[path]
-        sns.lineplot(
-            X_path,
-            x=ft1,
-            y=ft2,
-            sort=False,
-            ax=ax,
-            label=f"Path {i}",
-            lw=2,
-            path_effects=[
-                patheffects.SimpleLineShadow((0.5, -0.5), "k", 0.5),
-                patheffects.Normal(),
-            ],
-            alpha=0.8,
-        )
-
-    ax.set_xlabel(ax.get_xlabel().upper())
-    ax.set_ylabel(ax.get_ylabel().upper())
-    plt.show()
 
 
 def main(verbose: bool = True) -> None:
@@ -211,15 +116,17 @@ def main(verbose: bool = True) -> None:
             verbose=verbose,
         )
 
+        pca = PCA(n_components=2)
+        pca.fit(X_std)
+
         if not paths:
             warn("No paths found!")
 
         for path in paths:
             print(X_raw.loc[X.index[path]])
 
-        pca = PCA(n_components=2)
-        pca.fit(X_std)
         plot_images(X_std, y, paths, pca, model)
+        plt.show()
 
     key = "cost"
     X_raw, y_raw = load_dataframe(verbose=verbose)
--- src/helper/adult.py
+++ src/helper/adult.py
@@ -1,7 +1,13 @@
 from pathlib import Path
-from typing import Any
+from typing import Any, Protocol
 
+import numpy as np
 import pandas as pd
+import seaborn as sns
+from matplotlib import patheffects
+from matplotlib import pyplot as plt
+from numpy.typing import NDArray
+from sklearn.decomposition import PCA
 
 from helper.common import select_mask
 
@@ -9,6 +15,10 @@ PATH = "dataset/50Ktrain.csv"
 DROPS = ["fnlwgt", "education", "marital-status", "relationship"]
 IMMUTABLES = ["sex", "race", "occupation", "native-country"]
 YCOL = "50K"
+
+
+class SupportsPredictProba(Protocol):
+    def predict_proba(self, X: Any) -> NDArray: ...
 
 
 def load_dataframe(
@@ -43,3 +53,92 @@ def select_actionable[*T](
     if verbose:
         print(f"Actionable dataset with size {res[0].shape[0]}")
     return res
+
+
+def non_outliers_mask(X: pd.DataFrame, threshold: float) -> NDArray:
+    return (X.abs() <= threshold).all(1).to_numpy()
+
+
+def proba_argsort(model: SupportsPredictProba, X: pd.DataFrame) -> NDArray:
+    return model.predict_proba(X)[:, 1].argsort()
+
+
+def get_targets(y: pd.Series, label: int) -> list[int]:
+    return (y == label).to_numpy().nonzero()[0].tolist()
+
+
+def plot_images(
+    X: pd.DataFrame,
+    y: pd.Series,
+    paths: list[list[int]],
+    component: PCA,
+    model: SupportsPredictProba,
+) -> None:
+    ax: plt.Axes  # type: ignore
+    fig, ax = plt.subplots()
+    X_2d: pd.DataFrame = component.transform(X)  # type: ignore
+    ft1, ft2 = component.get_feature_names_out()
+    sns.scatterplot(
+        X_2d,
+        x=ft1,
+        y=ft2,
+        hue=y,
+        hue_norm=(0, 1),
+        size=y,
+        sizes=(6, 24),
+        size_order=(1, 0),
+        style=y,
+        ax=ax,
+        palette="coolwarm",
+        alpha=0.9,
+        ec="w",
+        lw=0.2,
+        zorder=len(paths) + 2,
+    )
+
+    bound = ax.axis()
+    step = 256
+    j = complex(0, step)
+    grid = np.mgrid[bound[0] : bound[1] : j, bound[2] : bound[3] : j]
+    X_grid = pd.DataFrame(
+        component.inverse_transform(grid.reshape(2, step * step).T),
+        columns=component.feature_names_in_,
+    )
+    y_grid = model.predict_proba(X_grid)[:, 1].reshape(step, step)
+    ct = ax.contourf(
+        grid[0],
+        grid[1],
+        y_grid,
+        levels=10,
+        vmin=0,
+        vmax=1,
+        alpha=1 / 3,
+        zorder=0,
+        cmap="coolwarm",
+    )
+    cb = fig.colorbar(
+        ct,
+        ax=ax,
+        label="Probability",
+    )
+    cb.solids.set_alpha(1)  # type: ignore
+
+    for i, path in enumerate(paths):
+        X_path = X_2d.iloc[path]
+        sns.lineplot(
+            X_path,
+            x=ft1,
+            y=ft2,
+            sort=False,
+            ax=ax,
+            label=f"Path {i}",
+            lw=2,
+            path_effects=[
+                patheffects.SimpleLineShadow((0.5, -0.5), "k", 0.5),
+                patheffects.Normal(),
+            ],
+            alpha=0.8,
+        )
+
+    ax.set_xlabel(ax.get_xlabel().upper())
+    ax.set_ylabel(ax.get_ylabel().upper())
